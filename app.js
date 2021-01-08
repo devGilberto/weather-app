7b7ea70c1261b3d4485e5bb3045bc09043e2e18a
devGilberto <gmrf1996@gmail.com>
2021-01-08 15:32:41 +0000

    Callback Function And Callback Abstration

--- app.js
+++ app.js
@@ -1,57 +1,89 @@
 const request = require('postman-request');
+const geocode = require('./utils/geocode'); // optional file extension -> ./utils/geocode.js
 
 if(process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-const ak = process.env.ACCESS_KEY_WEATHERSTACK;
-//const url = 'http://api.weatherstack.com/current?access_key=' + ak + '&query=39.154444,-9.066111';
-const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=';
-// const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=39.154444,-9.06611&forecast_days =7&hourly=0&units=m';
-// const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=' + lat +',' + long + '&forecast_days =7&hourly=0&units=m';
+// const ak = process.env.ACCESS_KEY_WEATHERSTACK;
+// //const url = 'http://api.weatherstack.com/current?access_key=' + ak + '&query=39.154444,-9.066111';
+// const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=';
+// // const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=39.154444,-9.06611&forecast_days =7&hourly=0&units=m';
+// // const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=' + lat +',' + long + '&forecast_days =7&hourly=0&units=m';
 
-request({url: url, json: true}, (error, response) => {  //,body
-    if(error){
-        console.log("Unable to connect to weather service!");
-    }
-        else if(response.body.error.code){
-        // else if(response.body.error.code === 601){
-            console.log("Unable to find location");
-        }
+// request({url: url, json: true}, (error, response) => {  //,body
+//     if(error){
+//         console.log("Unable to connect to weather service!");
+//     }
+//         else if(response.body.error.code){
+//         // else if(response.body.error.code === 601){
+//             console.log("Unable to find location");
+//         }
 
-    else{
-        // console.log(response.body.current)
-        console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out.");
-    }
+//     else{
+//         // console.log(response.body.current)
+//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out.");
+//     }
    
-});
+// }); */
 
 
-//Geocoding
-//Address -> Lat/Long -> Weather
-const at = process.env.ACCESS_TOKEN_MAPBOX;
+// //Geocoding
+// //Address -> Lat/Long -> Weather
+// const at = process.env.ACCESS_TOKEN_MAPBOX;
 
-const urlMapbox = 'https://api.mapbox.com/geocoding/v5//mapbox.places/12what.json?access_token=' + at + '&limit=1';
-// const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + 'Los%20Angeles' + '.json?access_token=' + at + '&limit=1';
-// var lat, long = '';
-request({url: urlMapbox, json: true}, (error, response) => {  //,body
-    if(error){
-        console.log("Unable to connect to location services!");
-    }
-        else if(response.body.features.length === 0){
-            console.log("Unable to find location. Try another search.");
-        }
+// const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/lisbon.json?access_token=' + at + '&limit=1';
+// // const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + 'Los%20Angeles' + '.json?access_token=' + at + '&limit=1';
+// // var lat, long = '';
+// request({url: urlMapbox, json: true}, (error, response) => {  //,body
+//     if(error){
+//         console.log("Unable to connect to location services!");
+//     }
+//         else if(response.body.features.length === 0){
+//             console.log("Unable to find location. Try another search.");
+//         }
 
-    else
-    {
-        // console.log(response.body.current)
-        //console.log(response.body.features[0].center);
-        // coordinates = response.body.features[0].center;
-        const lat = response.body.features[0].center[0];
-        const long = response.body.features[0].center[1];
+//     else
+//     {
+//         // console.log(response.body.current)
+//         //console.log(response.body.features[0].center);
+//         // coordinates = response.body.features[0].center;
+//         const lat = response.body.features[0].center[0];
+//         const long = response.body.features[0].center[1];
 
-        console.log(lat, long);
-    }
-});
+//         console.log(lat, long);
+
+//         //---------------------------------------------------------------------------------------------------------------
+        
+// /*         const ak = process.env.ACCESS_KEY_WEATHERSTACK;
+//         //const url = 'http://api.weatherstack.com/current?access_key=' + ak + '&query=39.154444,-9.066111';
+//         const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=';
+//         // const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=39.154444,-9.06611&forecast_days =7&hourly=0&units=m';
+//         // const url = 'http://api.weatherstack.com/forecast?access_key=' + ak + '&query=' + lat +',' + long + '&forecast_days =7&hourly=0&units=m';
+
+//         request({url: url, json: true}, (error, response) => {  //,body
+//             if(error){
+//                 console.log("Unable to connect to weather service!");
+//             }
+//                 else if(response.body.error.code){
+//                 // else if(response.body.error.code === 601){
+//                     console.log("Unable to find location");
+//                 }
+
+//             else{
+//                 // console.log(response.body.current)
+//                 console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out.");
+//             }
+   
+//         }); */
+
+//     }
+// });
 
 
+
+
+geocode('Boston', (error, data) => {
+    console.log('Error ', error)
+    console.log('Data ', data)
+})
---
+++ utils/geocode.js
@@ -0,0 +1,29 @@
+const request = require('postman-request');
+
+
+const geocode = (address, callback) => {
+    const at = process.env.ACCESS_TOKEN_MAPBOX;
+    //encodeURIComponent(address)
+    const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address  + '.json?access_token=' + at + '&limit=1';
+
+    request({url: urlMapbox, json: true}, (error, response) => {
+
+        if(error){
+            callback('Unable to connect to location services!', undefined)
+        } 
+            else if(response.body.features.length === 0){
+                callback('Unable to find location. Try another search.', undefined)        
+            }
+          
+        else{
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })     
+        }
+    })
+
+}
+
+module.exports = geocode;
